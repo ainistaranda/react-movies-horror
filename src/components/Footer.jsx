8cabd5075c7b5b3deff7c5c4d1fce973f7cc6299
Ainis Taranda <taranda.ainis@gmail.com>
2022-10-27 16:16:14 -0400

    horror movie site

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <footer>
+            <p>&copy; 2022 Ainis Taranda. 
+                Source: <a href="https://sampleapis.com/api-list/movies">sampleapis.com</a></p>
+        </footer>
+    )
+}
+export default Footer
